6c82d5845b10b7fe3e574fd2d9a0b7f9241d5469
Josue Tori <josuearr.t@gmail.com>
2023-08-16 18:11:05 -0500

    consulta 2

--- main.py
+++ main.py
@@ -11,5 +11,15 @@ def c1():
     print(result.to_string())
     print(set(years))
 
+def c2():
+    brazil_argentina_matchess = matches_data[
+        ((matches_data['Home Team Name'] == 'Argentina') & (matches_data['Away Team Name'] == 'Brazil')) |
+        ((matches_data['Home Team Name'] == 'Brazil') & (matches_data['Away Team Name'] == 'Argentina'))
+        ]
+
+    cant = len(brazil_argentina_matchess)
+    print(brazil_argentina_matchess)
+    print(f"Se enfrentaron {cant} veces")
 
 c1()
+c2()
